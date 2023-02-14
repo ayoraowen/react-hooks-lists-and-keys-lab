98ec0469c2224aff02931a3124b232f63d27c295
owenayora <ayoraowen@gmail.com>
2023-02-14 20:12:13 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  const allLinks=links.map((linkString)=><a href="#home" key={linkString}>{linkString}</a>)
+  const allLinks=links.map((linkString)=><a href={`#${linkString}`} key={linkString}>{linkString}</a>)
 
   return <nav>{allLinks}</nav>;
 }
